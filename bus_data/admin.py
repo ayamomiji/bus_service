3ec16f3727091725c632070e5fc2b211944bc5d1
ayaya zhao <486841+ayamomiji@users.noreply.github.com>
2023-10-12 04:11:29 +0800

    add models to admin

--- bus_data/admin.py
+++ bus_data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Route, RouteStop, Stop
 
-# Register your models here.
+
+admin.site.register(Route)
+admin.site.register(Stop)
+admin.site.register(RouteStop)
