c9f266ac2a78d3ac46ffa9cbc864235811af380c
ayaya zhao <486841+ayamomiji@users.noreply.github.com>
2023-10-12 03:29:19 +0800

    define bus models

--- bus_data/models.py
+++ bus_data/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Route(models.Model):
+    name = models.CharField(max_length=50)
+    # Departure = 去程, Return = 回程
+    direction = models.CharField(max_length=50)
+
+
+class Stop(models.Model):
+    name = models.CharField(max_length=50)
+
+
+# 增加 default_scope, 按照 order 排序
+class RouteStopManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by("order")
+
+
+class RouteStop(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
+    order = models.IntegerField(db_index=True)
+
+    objects = RouteStopManager()
